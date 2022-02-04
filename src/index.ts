a9514c7c9a06fc6ef47acc55aa3778db7c94af98
Dup4 <lyuzhi.pan@gmail.com>
2022-02-05 07:51:01 +0800

    chore: release v1.8.4

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as Config } from "@/lib/config";
 export { default as Problem } from "@/lib/problem";
 export { default as Contest } from "@/lib/contest";
 export { default as Submission } from "@/lib/submission";
