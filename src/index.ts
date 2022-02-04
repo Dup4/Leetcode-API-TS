7d26f28cdef14d4221bb001775ee290ed264eff1
Dup4 <lyuzhi.pan@gmail.com>
2022-02-04 14:28:06 +0800

    fix: paths ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "@/lib/problem";
-export * from "@/lib/contest";
-export * from "@/lib/submission";
+export { default as Problem } from "@/lib/problem";
+export { default as Contest } from "@/lib/contest";
+export { default as Submission } from "@/lib/submission";
 export * from "@/utils/interfaces";
 
 export { default as default } from "@/lib/leetcode";
